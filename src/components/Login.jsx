014a3f67c8d6089c7a64e5322c48e5baa5e29764
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-20 10:13:10 +0600

    Implement login and registration forms with input handling; add navigation links for user flow

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Login() {
+    const handleLogin = e=>{
+        e.preventDefault()
+        const name= e.target.name.value
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name,email,password);
+    }
   return (
     <div>
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
+        <div className="hero bg-base-200 ">
+            <div className="hero-content flex-col ">
+                <div className="text-center ">
                 <h1 className="text-5xl font-bold">Login now!</h1>
-                <p className="py-6">
-                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                    quasi. In deleniti eaque aut repudiandae et a id nisi.
-                </p>
+                
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <div className="card-body">
-                    <form className="fieldset">
-                    <label className="label">Email</label>
-                    <input type="email" name='email' className="input" placeholder="Email" />
-                    <label className="label">Password</label>
-                    <input type="password" name='password' className="input" placeholder="Password" />
-                    <div><a className="link link-hover">Forgot password?</a></div>
-                    <button className="btn btn-neutral mt-4">Login</button>
+                    <form className="fieldset" onSubmit={handleLogin}>
+                        <label className="label">Name</label>
+                        <input type="text" name='name' className="input" placeholder="Enter your name" required/>
+
+                        <label className="label">Email</label>
+                        <input type="email" name='email' className="input" placeholder="Email" required/>
+
+                        <label className="label">Password</label>
+                        <input type="password" required name='password' className="input" placeholder="Password" />
+
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-neutral mt-4">Login</button>
                     </form>
+                    <p>New here? Please <Link to="/register">
+                        <button className='btn btn-link'>Register</button>
+                    </Link></p>
                 </div>
                 </div>
             </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Register() {
+  const handleRegister = e=>{
+        e.preventDefault()
+        const name= e.target.name.value
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(name,email,password);
+    }
   return (
     <div>
-        
+        <div className="hero bg-base-200 ">
+            <div className="hero-content flex-col ">
+                <div className="text-center ">
+                <h1 className="text-5xl font-bold">Register now!</h1>
+                
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                <div className="card-body">
+                    <form className="fieldset" onSubmit={handleRegister}>
+                        <label className="label">Name</label>
+                        <input type="text" name='name' className="input" placeholder="Enter your name" required/>
+
+                        <label className="label">Email</label>
+                        <input type="email" name='email' className="input" placeholder="Email" required/>
+
+                        <label className="label">Password</label>
+                        <input type="password" required name='password' className="input" placeholder="Password" />
+
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-neutral mt-4">Login</button>
+                    </form>
+                    <p>Already have an account? Please <Link to="/login">
+                        <button className='btn btn-link'>Login</button>
+                    </Link></p>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
